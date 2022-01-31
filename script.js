6adb4a3848ca91f4096a946de938dd802a35de3c
Gatsby <bokmanwd@gmail.com>
2022-01-31 14:59:02 -0700

    recopied and pated index to find my mistake

--- script.js
+++ script.js
@@ -17,4 +17,47 @@ if (minute < 10) {
 }
 let time = hour + ':' + minute;
 currentDay.text(`It's ` + time + ' on ' + date);
+setInterval(function() {
+        //Date
+        date = moment().format('MMMM DD, YYYY');
+        //Time
+        currentDay.text(`It's ` + time + ' on ' + date);
+        hour = moment().get('hour');
+        // Sets Present Class
+    $('.time-row').each(function() {
+        let row = parseInt($(this).attr('id'));
+        if (hour === row) {
+            $(this).addClass('present').removeClass('past future');
+        } else if (row < hour) {
+            $(this).addClass('past').removeClass('present future');
+        } else if (row > hour) {
+            $(this).addClass('future').removeClass('past present');
+        }
+    });
+    // 12 hour time setup
+    let HOUR = hour;
+    if (hour>12) {
+        hour = hour - 12
+    }
+    if (HOUR >= 12) {
+        time = hour + ':' + minute + 'pm';
+    } else if (HOUR <= 12) {
+        time = hour + ':' + minute + 'am';
+    }
+    // Minutes
+    minute = moment().get('minute');
+    if (minute < 10)  {
+        minute = '0' + minute
+    }
 
+}, 1000);
+
+$('.btn').click(function() {
+    localStorage.setItem($(this).parent('.time-row').attr('id'), $(this).siblings('.fullWidth').children().val());
+});
+
+$(document).ready(function() {
+    for (let i = 9; i < 18; i++) {
+        $(`#${i}`).children('.fullWidth').children('.event').text(localStorage.getItem(i));
+    }
+});
